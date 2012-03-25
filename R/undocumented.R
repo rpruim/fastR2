03fb7bfcbb360cfacd6e1e6939c859a263eb0ca6
Randall Pruim <rpruim@gmail.com>
2012-03-25 00:38:40 -0400

    adding a couple files to repos

---
+++ R/undocumented.R
@@ -0,0 +1,16 @@
+#' Undocumented functions
+#' 
+#' Documentation is still pending for this functions
+#' 
+#' @rdname undocumented
+#' @name undocumented
+#'
+#' @aliases tdf funplot grid.identify.points ntigram
+#' panel.lm panel.lmbands panel.smooth panel.xyplotpoints panel.xyplotsmooth
+#' pdfplot pmfhistogram pmfplot probplot stemplot stemplot.data.frame
+#' stemplot.numeric funvec givenOrder make.rm multirandom nlmax nlmin
+#' summary.nlmax summary.nlmin print.stemplot show.fumbles show.poisson
+#' @author Randall Pruim
+
+NULL
+
