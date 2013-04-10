d59983b8186fc73029e8326b3da54f37cd97a4fa
Randall Pruim <rpruim@gmail.com>
2013-04-10 16:10:20 -0400

    added some messaging

--- bin/submit.sh
+++ bin/submit.sh
@@ -1,5 +1,15 @@
 # provide package.tar.gz as argument
-
+echo ""
+echo "file to be submitted: $1"
+echo ""
 echo "ftp  -u ftp://cran.r-project.org/incoming/$1 $1"
 ftp  -u ftp://cran.r-project.org/incoming/$1 $1
+
+echo ""
+echo "A submission should be accompanied by an email to CRAN@R-project.org,"
+echo "if possible sent from the maintainer address listed in the package,"
+echo "using a subject line of the form"
+echo ""
+echo "    CRAN submission PACKAGE VERSION"
+echo ""
 
