cc9249b22fdb14eef1535840f6d1009c8b70270f
Randall Pruim <rpruim@gmail.com>
2013-04-16 20:49:46 -0400

    adding --as-cran to R CMD check

--- bin/checkpackage.sh
+++ bin/checkpackage.sh
@@ -3,4 +3,4 @@ export _R_CHECK_FORCE_SUGGESTS_=false
 bin/roxy -p "." --clean
 mv *_*.tar.gz builds/
 R CMD build --resave-data .
-bin/do2all "R CMD check %p" *_*.tar.gz
+bin/do2all "R CMD check --as-cran %p" *_*.tar.gz
