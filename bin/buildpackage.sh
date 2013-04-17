ca6286ff7a95c7b248f0bbf2eae728dd39b81ba8
Randall Pruim <rpruim@gmail.com>
2013-04-16 21:01:07 -0400

    restructuring directories

--- bin/buildpackage.sh
+++ bin/buildpackage.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 export _R_CHECK_FORCE_SUGGESTS_=false
-bin/roxy -p "." --clean
+bin/roxy -p "package" --clean
 mv *_*.tar.gz builds/
-R CMD build --resave-data .
+R CMD build --resave-data package
--- bin/checkpackage.sh
+++ bin/checkpackage.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 export _R_CHECK_FORCE_SUGGESTS_=false
-bin/roxy -p "." --clean
+bin/roxy -p "package" --clean
 mv *_*.tar.gz builds/
-R CMD build --resave-data .
+R CMD build --resave-data package
 bin/do2all "R CMD check --as-cran %p" *_*.tar.gz
