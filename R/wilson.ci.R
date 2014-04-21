ee28e5e0ec55da0e59995f7d867ed21aad5f4c34
Randall Pruim <rpruim@gmail.com>
2014-04-21 18:51:41 -0400

    bug fix: n -> n+4

--- R/wilson.ci.R
+++ R/wilson.ci.R
@@ -32,7 +32,7 @@ function (x, n = 100, conf.level = 0.95)
     alpha = 1 - conf.level
     p = (x + 2)/(n + 4)
     zstar <- -qnorm(alpha/2)
-    interval <- p + c(-1, 1) * zstar * sqrt(p * (1 - p)/n)
+    interval <- p + c(-1, 1) * zstar * sqrt(p * (1 - p)/(n+4))
     attr(interval, "conf.level") <- conf.level
     return(interval)
 }
