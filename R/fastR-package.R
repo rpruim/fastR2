13f53a175524f1b16c01d4fa1f7c9b96d5d0876a
Randall Pruim <rpruim@gmail.com>
2015-12-12 16:17:45 -0500

    add a bunch of iports

--- R/fastR-package.R
+++ R/fastR-package.R
@@ -22,6 +22,13 @@
 #' @import mosaicData
 #' @import lattice
 #' @import grid
+#' @importFrom grDevices as.graphicsAnnot colorRampPalette dev.interactive devAskNewPage 
+#' @importFrom grDevices extendrange gray heat.colors
+#' @importFrom graphics abline axis box boxplot lines mtext plot points
+#' @importFrom stats coef cooks.distance deviance df.residual dnorm effects 
+#' @importFrom stats fitted lm lm.influence model.frame nlm ppoints predict qnorm residuals rnorm terms weights
+#' @importFrom utils file_test
+
 NULL
 
 
