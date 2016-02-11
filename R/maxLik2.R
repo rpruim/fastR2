a131c153a029a2987d11ad042735d8960a48475f
Randall Pruim <rpruim@gmail.com>
2016-02-10 21:48:18 -0500

    add maxLik stuff

---
+++ R/maxLik2.R
@@ -0,0 +1,75 @@
+#' Augmented version of maxLik
+#' 
+#' This version of \code{\link{maxLik}} stores additional information in the 
+#' returned object enabling a plot method.
+#' 
+#' @import maxLik
+#' 
+#' @param loglik a log-likelihood function as for \code{\link{maxLik}}
+#' @param ... additional arguments passed to \code{\link{maxLik}}
+#' 
+#' @export
+
+maxLik2 <- function(loglik, ...) {
+  orig.call <- match.call()
+  fn <- loglik
+  dots <- list(...)
+  env <- new.env()
+  
+  result <- maxLik::maxLik(loglik, ...) 
+  for (n in intersect(names(dots), names(formals(fn)))) {
+    formals(fn)[[n]] <- NULL
+    assign(n, dots[[n]], env)
+  }
+  environment(fn) <- env
+  result$loglik <- fn
+  class(result) <- c("maxLik2", class(result))
+  result
+}
+
+#' plot method for augment maxLik objects
+#' 
+#' See \code{\link{maxLik2}} and \code{\link{maxLik}} for how to create
+#' the objects this method prints.
+#' 
+#' @param x
+#' @param y
+#' @export
+
+plot.maxLik2 <- function(x, y, ...) {
+  ml <- x
+  
+  switch(
+    length(coef(ml)), 
+    "1" = {
+      se <- stdEr(ml)
+      S <- Vectorize(function(.x) {   numDeriv::grad(   Vectorize(ml$loglik), .x) })
+      I <- Vectorize(function(.x) { - numDeriv::hessian(Vectorize(ml$loglik), .x) })
+      Q <- function(.x) { ml$loglik(coef(ml)) - 1/2 * I(coef(ml)) *(.x - coef(ml))^2}
+      G <- data.frame(theta = c(coef(ml) - 3 * se, coef(ml) + 3 * se))
+      ggplot(G, aes(x = theta)) + 
+        stat_function(fun = ml$loglik, size = 1.2) +
+        stat_function(fun = Q, colour = "skyblue", size = 0.8, alpha = 0.7) +
+        geom_vline(xintercept = coef(ml) - 2 * se, colour = "skyblue", linetype = "dashed") +
+        geom_vline(xintercept = coef(ml), colour = "skyblue", linetype = "dotted", size = 1) +
+        geom_vline(xintercept = coef(ml) + 2 * se, colour = "skyblue", linetype = "dashed") +
+        labs(x = names(coef(ml)), y = "log-likelihood")
+    },
+    "2" = {
+      se <- stdEr(ml)
+      est <- coef(ml)
+      G <- expand.grid(
+        seq(est[1] - 3 * se[1], est[1] + 3 * se[1], length.out = 50),
+        seq(est[2] - 3 * se[2], est[2] + 3 * se[2], length.out = 50)
+      ) 
+      names(G) <- c("p1", "p2")
+      G$loglik <- apply(G, 1, ml$loglik)
+      levelplot( loglik ~ p1 + p2, data = G, contour = TRUE, 
+                 col.regions = topo.colors(100),
+                 xlab = names(coef(ml))[1],
+                 ylab = names(coef(ml))[2]
+      )
+    },
+    default = stop("Plotting only defined for likelihoods on 1 or 2 parameters")
+  )
+}
