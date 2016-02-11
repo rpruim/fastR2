a01e6c7bf7e1644d6061083726641d135d148f5f
Randall Pruim <rpruim@gmail.com>
2016-02-10 21:48:00 -0500

    another snippet added

---
+++ inst/snippet/Joes-coin3.R
@@ -0,0 +1,12 @@
+score.ci <- function(x, n, level=0.95) {
+    alpha = 1 - level;
+    z.star <- qnorm(1 - alpha/2);
+    pi.hat <- x/n;
+    A <- pi.hat + z.star^2/(2*n);
+    B <- z.star * sqrt( ( pi.hat * (1-pi.hat) / n ) 
+	                  + ( z.star^2/(4 * n^2) ) );
+    D <- 1 + z.star^2/n;
+    # interval is ( A +- B) / D
+    ( A + c(-1,1) * B ) / D;
+}
+
