e520c3f568a3f9ea989cf6da373abbc721d91b3e
Randall Pruim <rpruim@gmail.com>
2016-08-09 11:23:12 -0400

    NCAA BB data from kenpom

---
+++ data-raw/NCAAfetch.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+wget http://kenpom.com/cbbga07.txt 
+wget http://kenpom.com/cbbga08.txt 
+wget http://kenpom.com/cbbga09.txt 
+wget http://kenpom.com/cbbga10.txt 
+wget http://kenpom.com/cbbga11.txt 
+wget http://kenpom.com/cbbga12.txt 
+wget http://kenpom.com/cbbga13.txt 
+wget http://kenpom.com/cbbga14.txt 
+wget http://kenpom.com/cbbga15.txt 
+wget http://kenpom.com/cbbga16.txt 
+
+cat kenpom-games-header.txt cbbga*txt > ncaa-bb.txt
+
